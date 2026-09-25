c39f3c175eb5d59c94a5947f7ae12292746e6439
agillam69 <62461562+agillam69@users.noreply.github.com>
2026-09-25 13:56:36 +0800

    Update HWRT user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,11 +26,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'username' => fake()->unique()->userName(),
+            'email' => fake()->optional()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => 'logger',
+            'role' => 'user',
             'active' => true,
         ];
     }
